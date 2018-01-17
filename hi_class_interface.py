95c5d35f8a635031ef1d370e94bf979f95c78e42
dinatraykova <dina.traykova.13@ucl.ac.uk>
2018-01-17 14:40:36 +0000

    added some more options

--- hi_class_interface.py
+++ hi_class_interface.py
@@ -18,7 +18,6 @@ import numpy as np
 cosmo = names.cosmological_parameters
 distances = names.distances
 cmb_cl = names.cmb_cl
-#configpars.read('test/test_vals_p.ini')
 
 def setup(options):
     #Read options from the ini file which are fixed across
@@ -28,11 +27,12 @@ def setup(options):
         'zmax': options.get_double(option_section,'zmax', default=4.0),
         'kmax': options.get_double(option_section,'kmax', default=50.0),
         'debug': options.get_bool(option_section, 'debug', default=False),
-        'lensing': options.get_string(option_section, 'lensing', default = 'no'),
-        'expansion_model': options.get_string(option_section, 'expansion_model', default = 'lcdm'),
-        'gravity_model':   options.get_string(option_section, 'gravity_model', default = 'propto_omega'),
+        'lensing': options.get_string(option_section, 'lensing', default = 'yes'),
+#        'expansion_model': options.get_string(option_section, 'expansion_model', default = 'lcdm'),
+#        'gravity_model':   options.get_string(option_section, 'gravity_model', default = 'propto_omega'),
         'modes':  options.get_string(option_section, 'modes', default = 's'),
-        'output': options.get_string(option_section, 'output', default = 'tCl,pCl'),
+        'output': options.get_string(option_section, 'output', default = 'tCl,lCl,pCl,mPk'),
+        'sBBN file': options.get_string(option_section, 'sBBN file', default = '${COSMOSIS_SRC_DIR}/modules/hi_class/hi_class_devel/bbn/sBBN.dat'),
             }
     #Create the object that connects to Class
     config['cosmo'] = classy.Class()
@@ -49,28 +49,31 @@ def get_class_inputs(block, config):
         'output':        config["output"],
         'modes':         config["modes"],
         'l_max_scalars': config["lmax"],
-        #'P_k_max_h/Mpc': config["kmax"],
+        'P_k_max_h/Mpc': config["kmax"],
         'lensing':       config["lensing"],
-        #'z_pk': ', '.join(str(z) for z in np.arange(0.0, config['zmax'], 0.01)),
+        'z_pk': ', '.join(str(z) for z in np.arange(0.0, config['zmax'], 0.01)),
         'n_s':          block[cosmo, 'n_s'],
-#        'H0':           100*block[cosmo, 'h0'],
         'omega_b':      block[cosmo, 'ombh2'],
         'omega_cdm':    block[cosmo, 'omch2'],
         'tau_reio':     block[cosmo, 'tau'],
         'T_cmb':        block.get_double(cosmo, 't_cmb', default=2.726),
-        'N_eff':        block.get_double(cosmo, 'massless_nu', default=3.046),
-        #'r':            block.get_double(cosmo, 'r_t', default=0.),
-#        '100*theta_s':  block[cosmo, '100*theta_s'],
+        'N_ur':         block.get_double(cosmo, 'N_ur', default=3.046),
+        'k_pivot':      block.get_double(cosmo, 'k_pivot', default=0.05),
+#        'N_ncdm':       block.get_double(cosmo, 'N_ncdm', default=0.),
+#        'm_ncdm':       block.get_double(cosmo, 'm_ncdm', default=0.),
+#        'T_ncdm':       block.get_double(cosmo, 'T_cdm', default=0.71611),
         }
     #print block.keys()
+#    if config.has_value('sBBN file'):
+    params['sBBN file'] = config['sBBN file']
     if block.has_value(cosmo, '100*theta_s'):
         params['100*theta_s'] = block[cosmo, '100*theta_s']
     if block.has_value(cosmo, 'h0'):
         params['H0'] = 100*block[cosmo, 'h0']
     if block.has_value(cosmo, 'A_s'):
         params['A_s'] = block[cosmo, 'A_s']
-    if block.has_value(cosmo, 'log1e10As'):
-        params['ln10^{10}A_s'] = block[cosmo, 'log1e10As']
+    if block.has_value(cosmo, 'logA'):
+        params['ln10^{10}A_s'] = block[cosmo, 'logA']
     if block.has_value(cosmo, 'omega_smg') and block[cosmo,'omega_smg'] < 0.:
         smgs = smg_params(block)
         params['expansion_model'] = config['expansion_model']
@@ -80,7 +83,10 @@ def get_class_inputs(block, config):
         params['Omega_fld'] = block.get_double(cosmo, 'omega_sfld', default = 0.)
         params['expansion_smg'] = block.get_double(cosmo, 'expansion_smg', default = 0.5)
         params['parameters_smg'] = block.get_string(cosmo, 'parameters_smg', default = smgs)
-
+    if block.has_value(cosmo, 'N_ur') and block[cosmo,'N_ur'] != 3.046:
+        params['N_ncdm'] = block[cosmo, 'N_ncdm']
+        params['m_ncdm'] = block[cosmo, 'm_ncdm']
+        params['T_ncdm'] = block[cosmo, 'T_ncdm']
     return params
 
 def get_class_outputs(block, c, config):
@@ -88,8 +94,8 @@ def get_class_outputs(block, c, config):
     ## Derived cosmological parameters
     ##
 
-   # block[cosmo, 'sigma_8'] = c.sigma8()
-   # h0 = block[cosmo, 'h0']
+    block[cosmo, 'sigma_8'] = c.sigma8()
+    h0 = c.Hubble(0.)#block[cosmo, 'h0']
 
     ##
     ##  Matter power spectrum
@@ -97,24 +103,24 @@ def get_class_outputs(block, c, config):
 
     #Ranges of the redshift and matter power
     dz = 0.01
-   # kmin = 1e-4
-   # kmax = config['kmax']*h0
-   # nk = 100
+    kmin = 1e-4
+    kmax = config['kmax']*h0
+    nk = 100
 
     #Define k,z we want to sample
     z = np.arange(0.0, config["zmax"]+dz, dz)
-   # k = np.logspace(np.log10(kmin), np.log10(kmax), nk)
+    k = np.logspace(np.log10(kmin), np.log10(kmax), nk)
     nz = len(z)
 
     #Extract (interpolate) P(k,z) at the requested
     #sample points.
-   # P = np.zeros((nk, nz))
-   # for i,ki in enumerate(k):
-   #     for j,zj in enumerate(z):
-   #         P[i,j] = c.pk(ki,zj)
+    P = np.zeros((nk, nz))
+    for i,ki in enumerate(k):
+        for j,zj in enumerate(z):
+            P[i,j] = c.pk(ki,zj)
 
     #Save matter power as a grid
-    #block.put_grid("matter_power_lin", "k_h", k/h0, "z", z, "p_k", P*h0**3)
+    block.put_grid("matter_power_lin", "k_h", k/h0, "z", z, "p_k", P*h0**3)
 
     ##
     ##Distances and related quantities
@@ -148,10 +154,13 @@ def get_class_outputs(block, c, config):
     #t_cmb is in K, convert to mu_K, and add ell(ell+1) factor
     tcmb_muk = block[cosmo, 't_cmb'] * 1e6
     f = ell*(ell+1.0) / 2 / np.pi * tcmb_muk**2
+    f1 = ell*(ell+1.0) / 2 / np.pi
 
     #Save each of the four spectra
-    for s in ['tt','ee','te','bb']:
+    for s in ['tt','ee','te','bb','tp']:
         block[cmb_cl, s] = c_ell_data[s][2:] * f
+    for s in ['pp']:
+        block[cmb_cl, s] = c_ell_data[s][2:] * f1
 
 def execute(block, config):
     c = config['cosmo']
